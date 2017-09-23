22447f5a9cf0a1bb0314812abbd3964af16cf7ce
Raussi Sipi TTK17SP <sipiraussi@kamk.fi>
2017-09-24 01:04:43 +0300

    Please work with me

--- Assets/Scripts/PlaneRotation.cs
+++ Assets/Scripts/PlaneRotation.cs
@@ -3,22 +3,10 @@
 public class PlaneRotation : MonoBehaviour
 {
     private new GameObject camera;
-    private Material material;
-    public Sprite Sp;
-    public Sprite Sp2;
 
     void Awake()
     {
         camera = GameObject.FindWithTag("MainCamera");
-        material = GetComponent<Material>();
-    }
-
-    void Update()
-    {
-        if (Escape.IsRunning && (material.mainTexture == Sp))
-        {
-            Sp = Sp2;
-        }
     }
 
 	void LateUpdate ()
