e5a570980d7ceb5614adbcb4682411807ecc7ddc
Auerniitty Lauri TTK17SP <lauriauerniitty@kamk.fi>
2017-09-24 04:14:19 +0300

    Added HIRVI prefab, more fixes, more garbage scripts

---
+++ Assets/ChangeOnlyOneSprite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeOnlyOneSprite : MonoBehaviour {
+
+    public Sprite idle;
+    private SpriteRenderer rer;
+    //you can actually sue me now
+
+
+    void Start()
+    {
+
+        rer = GetComponent<SpriteRenderer>();
+
+    }
+
+
+    void change () {
+
+        rer.sprite = idle;
+
+	}
+}
